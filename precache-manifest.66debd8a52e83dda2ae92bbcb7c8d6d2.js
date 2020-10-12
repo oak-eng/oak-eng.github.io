0b439d120a2b4702eaf72aeaca3572a8b9923f12
Hakeem Angulu <hakeem.angulu@gmail.com>
2020-10-12 02:09:22 -0400

    Updates

---
+++ precache-manifest.66debd8a52e83dda2ae92bbcb7c8d6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5e7fbc7bd91fe2536461c224c45d5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e49ed420fb85a04fd5f",
+    "url": "/static/css/main.6b625c23.chunk.css"
+  },
+  {
+    "revision": "588e89531c6a60287ae8",
+    "url": "/static/js/2.968c1ee9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.968c1ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e49ed420fb85a04fd5f",
+    "url": "/static/js/main.6bcc09db.chunk.js"
+  },
+  {
+    "revision": "2c9692fb00de42ed001f",
+    "url": "/static/js/runtime-main.490a36af.js"
+  }
+]);
