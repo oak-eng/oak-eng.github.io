dcda275309eb02cc1f3bc23d85620eb0c698fcd2
Hakeem Angulu <hakeem.angulu@gmail.com>
2021-02-17 16:24:13 -0500

    integrate MailChimp

--- src/components/Extension.js
+++ src/components/Extension.js
@@ -1,9 +1,113 @@
-import React from "react";
-import { Container, Grid, Header, Icon, Image } from "semantic-ui-react";
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  Modal,
+} from "semantic-ui-react";
 
 import styles from "./Extension.module.css";
 
+function MailChimpSignup({ signupOpen, setSignupOpen }) {
+  return (
+    <Modal
+      onClose={() => setSignupOpen(false)}
+      onOpen={() => setSignupOpen(true)}
+      open={signupOpen}
+      trigger={<button className={styles["button"]}>be notified</button>}
+    >
+      <Modal.Header>be notified when the extension is released!</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <Header as="h4" textAlign="center">
+            by signing up below, you'll be one of the first to be notified about
+            our new browser extension, releasing in the next few days! <br />
+            (and don't worry, we won't spam your inbox)
+          </Header>
+          <div id="mc_embed_signup">
+            <form
+              action="https://myoaksystem.us1.list-manage.com/subscribe/post?u=0699eb05d8a193b27b646c8c0&amp;id=1aef75b28c"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              class="validate"
+              target="_blank"
+              novalidate
+            >
+              <div id="mc_embed_signup_scroll">
+                <div class="indicates-required">
+                  <span class="asterisk">*</span> indicates required
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-EMAIL">
+                    email address <span class="asterisk">*</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="EMAIL"
+                    class="required email"
+                    id="mce-EMAIL"
+                  />
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-FNAME">first name </label>
+                  <input type="text" name="FNAME" class="" id="mce-FNAME" />
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-LNAME">last name </label>
+                  <input type="text" name="LNAME" class="" id="mce-LNAME" />
+                </div>
+                <div id="mce-responses" class="clear">
+                  <div
+                    class="response"
+                    id="mce-error-response"
+                    style={{ display: "none" }}
+                  ></div>
+                  <div
+                    class="response"
+                    id="mce-success-response"
+                    style={{ display: "none" }}
+                  ></div>
+                </div>
+                <div
+                  style={{ position: "absolute", left: "-5000px" }}
+                  aria-hidden="true"
+                >
+                  <input
+                    type="text"
+                    name="b_0699eb05d8a193b27b646c8c0_1aef75b28c"
+                    tabindex="-1"
+                    value=""
+                  />
+                </div>
+                <div class="clear">
+                  <input
+                    type="submit"
+                    value="sign up"
+                    name="subscribe"
+                    id="mc-embedded-subscribe"
+                    className={styles["form-submit-button"]}
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="black" onClick={() => setSignupOpen(false)}>
+          no thanks
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+}
+
 export default function Extension({ mobile }) {
+  const [signupOpen, setSignupOpen] = useState(false);
   const rowClass = mobile ? styles["oak-row-mobile"] : styles["oak-row"];
 
   return (
@@ -17,15 +121,10 @@ export default function Extension({ mobile }) {
                 Black-owned and compatible with your hair as you shop
               </h1>
 
-              <div className={styles["marquee-button"]}>
-                <a
-                  href="http://consult.myoaksystem.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className={styles["button"]}>download now</button>
-                </a>
-              </div>
+              <MailChimpSignup
+                signupOpen={signupOpen}
+                setSignupOpen={setSignupOpen}
+              />
             </Grid.Column>
             <Grid.Column>
               <Image
@@ -117,15 +216,10 @@ export default function Extension({ mobile }) {
               </h1>
             </Grid.Column>
             <Grid.Column>
-              <div className={styles["marquee-button"]}>
-                <a
-                  href="http://consult.myoaksystem.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <button className={styles["button"]}>start today</button>
-                </a>
-              </div>
+              <MailChimpSignup
+                signupOpen={signupOpen}
+                setSignupOpen={setSignupOpen}
+              />
             </Grid.Column>
           </Grid>
         </Container>
